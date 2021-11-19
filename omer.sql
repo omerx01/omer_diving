67f0b1934957b8534f79d2f187b4b7709b63622d
omerx01 <49867199+omerx01@users.noreply.github.com>
2021-11-19 20:44:32 +0100

    uploading..

---
+++ omer.sql
@@ -0,0 +1,9 @@
+INSERT INTO items (name, label) VALUES 
+    ('silver', 'Silver Coins'),
+    ('oxygen_mask', 'Oxygen Mask'),
+    ('hammer', 'Hammer'),
+    ('minichest', 'Mini Chest'),
+    ('goldsilver', 'Gold Coins'),
+    ('coral', 'Coral')
+
+;
